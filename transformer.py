3cabe689563f5248179d2b71ced7568116a9eaed
윤두밈(Dumim Yoon) <yoondm@ncsoft.com>
2020-07-21 15:04:03 +0900

    Add: positional_encoding

--- transformer.py
+++ transformer.py
@@ -94,7 +94,26 @@ def encoder(in_dims:int, seq_dims:int, out_dims:int, head_dims:int, sa_dims:int,
     else:
         return result
 
+#region positional_encoding: https://github.com/jalammar/jalammar.github.io/blob/master/notebookes/transformer/transformer_positional_encoding_graph.ipynb
+def get_angles(pos, i, d_model):
+  angle_rates = 1 / np.power(10000, (2 * (i//2)) / np.float32(d_model))
+  return pos * angle_rates
 
+def positional_encoding(position, d_model):
+  angle_rads = get_angles(np.arange(position)[:, np.newaxis],
+                          np.arange(d_model)[np.newaxis, :],
+                          d_model)
+  
+  # apply sin to even indices in the array; 2i
+  angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+  
+  # apply cos to odd indices in the array; 2i+1
+  angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+    
+  pos_encoding = angle_rads[np.newaxis, ...]
+    
+  return pos_encoding
+#endregion
 
 from IPython import embed;embed()
 exit()
