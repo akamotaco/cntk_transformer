6ef4463c246483f1e62326c18ea153727be6e0bb
윤두밈(Dumim Yoon) <yoondm@ncsoft.com>
2020-07-21 16:32:53 +0900

    Fix: sequential self_attention_layer

--- transformer_seq.py
+++ transformer_seq.py
@@ -2,37 +2,37 @@
 # https://nlpinkorean.github.io/illustrated-transformer/
 import cntk as C
 
-IN_DIMS = 4
-OUT_DIMS = 3 # self attention size
-HEAD_DIMS = 8
-
-def self_attention_layer(in_dims:int, out_dims:int, name='self_attention') -> C.Function:
+def self_attention_layer(in_dims:int, out_dims:int, name='self_attention', as_block:bool = False) -> C.Function:
     sq_sa_dims = C.Constant(C.sqrt(out_dims).eval(), name='sq_dims')
 
     init = C.initializer.normal(1)
 
     X = C.placeholder(in_dims, name=name+'_ph')
-    W_Q = C.parameter((IN_DIMS, out_dims), init=init, name=name+'_q')
-    W_K = C.parameter((IN_DIMS, out_dims), init=init, name=name+'_k')
-    W_V = C.parameter((IN_DIMS, out_dims), init=init, name=name+'_v')
+    W_Q = C.parameter((in_dims, out_dims), init=init, name=name+'_q')
+    W_K = C.parameter((in_dims, out_dims), init=init, name=name+'_k')
+    W_V = C.parameter((in_dims, out_dims), init=init, name=name+'_v')
 
     q = X@W_Q
     k = X@W_K
     v = X@W_V
 
-    # score = C.reduce_sum(q*k) # dot
-    # result = C.softmax(score/SA_DIMS_SQ)
-
     score = C.times_transpose(q, k, name=name+'_score')
-    result = C.times(C.softmax(score/sq_sa_dims), v, name=name+'_sum')
+    div_k = score/sq_sa_dims
+    softmax = C.sequence.softmax(div_k, name=name+'_softmax')
+    softmax_value = C.element_times(softmax, v, name=name+'_softmax_value')
 
-    return C.as_block(result, [(X,X)], 'self_attention', 'self_attention_')
+    result = softmax_value
 
-def multi_headed_self_attention_layer(num_of_head:int, in_dims:int, seq_dims:int, out_dims:int, sa_dims:int, name='multi_headed_self_attention', as_block:bool = False) -> C.Function:
-    X = C.placeholder((seq_dims, in_dims), name=name+'_ph')
+    if as_block:
+        C.as_block(result, [(X,X)], 'self_attention', 'self_attention_')
+    else:
+        return result
+
+def multi_headed_self_attention_layer(in_dims:int, out_dims:int, num_of_head:int, name='multi_headed_self_attention', as_block:bool = False) -> C.Function:
+    X = C.placeholder(in_dims, name=name+'_ph')
     layers = []
     for i in range(num_of_head):
-        layers.append(self_attention_layer(IN_DIMS, OUT_DIMS, SA_DIMS, name=name+str(i)))
+        layers.append(self_attention_layer(in_dims, out_dims, name=name+str(i)))
     outputs = []
     for layer in layers:
         outputs.append(layer(X))
@@ -41,7 +41,7 @@ def multi_headed_self_attention_layer(num_of_head:int, in_dims:int, seq_dims:int
 
     init = C.initializer.normal(1)
 
-    W_O = C.parameter((IN_DIMS, concat.shape[1]), init=init, name=name+'_Wo')
+    W_O = C.parameter((in_dims, concat.shape[1]), init=init, name=name+'_Wo')
 
     result = C.times_transpose(concat, W_O, name='result')
 
@@ -121,21 +121,25 @@ def positional_encoding(position, d_model):
 from IPython import embed;embed(header='end')
 exit()
 
-
+IN_DIMS = 4 # size of tokens
+OUT_DIMS = 3 # size of self attention
+HEAD_DIMS = 8 # size of multi-headed self attention
 
 
 import numpy as np
 
-v = [np.ones(IN_DIMS),np.ones(IN_DIMS),np.ones(IN_DIMS)]
+v = np.array([ [1,0,0,0], [1,1,1,1], [0,1,0,0] ], np.float32) # seq
 X = C.sequence.input_variable(IN_DIMS)
 sa_layer = self_attention_layer(IN_DIMS, OUT_DIMS)
 
 model = sa_layer(X)
 print(model.eval({model.arguments[0]:v}))
 
-# mhsa_layer = multi_headed_self_attention_layer(HEAD_DIMS, IN_DIMS, OUT_DIMS, SA_DIMS, as_block=True)
+# mhsa_layer = multi_headed_self_attention_layer(IN_DIMS, OUT_DIMS, HEAD_DIMS, as_block=True)
 # model = mhsa_layer(X)
 # print(model.eval({model.arguments[0]:v}))
+
+
 
 # ff_layer = feed_forward_layer(IN_DIMS, SEQ_DIMS, OUT_DIMS)
 
