c3b6ebabb4e44388fce7e9ddf5ff28c59cc879ca
윤두밈(Dumim Yoon) <yoondm@ncsoft.com>
2020-07-27 17:24:11 +0900

    Add: gpt2 block (w/o position encoding)

--- gpt2.py
+++ gpt2.py
@@ -1,40 +1,17 @@
 # http://jalammar.github.io/illustrated-gpt2/
+# https://github.com/openai/gpt-2/blob/master/src/model.py
 import cntk as C
 import numpy as np
 from transformer import layer_normalization, triangular_matrix_seq
+
+@C.Function
+def gelu(x):
+    return 0.5*x*(1+C.tanh(np.sqrt(2/np.pi)*(x+0.044715*C.pow(x, 3))))
 
 def positional_encoding(token_dims:int, discount_factor:float=0.99):
     X = C.placeholder(token_dims, name='positional_encoding')
     encoder = C.layers.Recurrence(C.element_times,initial_state=1, return_full_state=True)(C.ones_like(X)*discount_factor)
     return C.stop_gradient(C.as_block(encoder, [(X,X)], 'positional_encoding','positional_encoding_'))
-
-# def self_attention(token_dims:int, sa_dims:int, mask_opt:bool = False, as_block:bool=False, name:str='self_attention'):
-#     X = C.placeholder(token_dims, dynamic_axes=(C.Axis.default_batch_axis(), C.Axis.default_dynamic_axis()) ,name=name)
-
-#     q = C.layers.Dense(sa_dims, name=name+'_q')(X)
-#     k = C.layers.Dense(sa_dims, name=name+'_k')(X)
-#     v = C.layers.Dense(sa_dims, name=name+'_v')(X)
-
-#     q_ = C.sequence.unpack(q, 0, True)
-#     k_ = C.sequence.unpack(k, 0, True)
-#     v_ = C.sequence.unpack(v, 0, True)
-
-#     scores = C.times_transpose(q_, k_, name=name+'_scores')
-
-#     if mask_opt:
-#         mask = triangular_matrix_seq(2)(X)
-#         inf_mask = -np.inf*(mask-0.5)
-#         scores = C.element_min(scores, inf_mask)
-    
-#     softmax = C.softmax(scores, name=name+'_softmax')
-#     attention = C.times(softmax, v_, name=name+'_attention')
-
-#     result = C.to_sequence_like(attention, X, name=name+'_seq')
-
-#     if as_block:
-#         return C.as_block(result, [(X,X)], name,name)
-    
-#     return result
 
 def gpt2_self_attention(token_dims:int, head_dims:int, mask_opt:bool = False, as_block:bool=False, name:str='self_attention'):
     X = C.placeholder(token_dims, dynamic_axes=(C.Axis.default_batch_axis(), C.Axis.default_dynamic_axis()) ,name=name)
@@ -77,6 +54,8 @@ def gpt2_self_attention(token_dims:int, head_dims:int, mask_opt:bool = False, as
 #region mask opt
         mask = triangular_matrix_seq(2)(X)
         inf_mask = -np.inf*(mask-0.5)
+        inf_mask = C.as_block(inf_mask,[(X,X)],'mask','mask')
+
         scaled = C.element_min(scaled, inf_mask)
 #endregion        
 
@@ -104,8 +83,8 @@ def gpt2_self_attention(token_dims:int, head_dims:int, mask_opt:bool = False, as
 def feed_forward_layer(hidden_dims:int, out_dims:int, as_block:bool = False, name:str='ff_layer'):
     X = C.placeholder(out_dims, dynamic_axes=(C.Axis.default_batch_axis(), C.Axis.default_dynamic_axis()) ,name=name)
 
-    ff = C.layers.Dense(hidden_dims,activation=C.elu)(X)
-    ff = C.layers.Dense(out_dims)(ff)
+    ff = C.layers.Dense((4,out_dims),activation=gelu, name='fc')(X)
+    ff = C.layers.Dense(out_dims, name='projection')(ff)
     result = ff
 
     if as_block:
@@ -123,8 +102,9 @@ def gpt2_block(token_dims:int, head_dims:int, as_block:bool = False, name:str='g
     ff_layer = feed_forward_layer(4*token_dims, token_dims)
 
     sa = sa_layer(layer_normalization(X))
+
     sa = X + sa
-    ff = ff_layer(sa)
+    ff = ff_layer(layer_normalization(sa))
     ff = X + ff
 
     result = ff
@@ -151,7 +131,6 @@ if __name__ == '__main__':
     VOCAB_DIMS = 50257
     TOKENS_DIMS = 768
     SEQ_DIMS = 1024
-    # SA_DIMS = 3
     HEAD_DIMS = 12
 
     Q = C.sequence.input_variable(VOCAB_DIMS, name='vocab_input')
@@ -159,8 +138,18 @@ if __name__ == '__main__':
     initial_pe = np.ones((TOKENS_DIMS, SEQ_DIMS))
     PE = C.parameter((TOKENS_DIMS, SEQ_DIMS), init=initial_pe ,name='positional_encoding')
 
-    # sa_layer = gpt2_self_attention(TOKENS_DIMS, HEAD_DIMS)
     block_layer = gpt2_block(TOKENS_DIMS, HEAD_DIMS)
     block = block_layer(E)
+
+    t = np.random.uniform(size=(5,VOCAB_DIMS)).astype(np.float32)
+
+    print(block.eval({Q:t}))
+
+    A = C.sequence.input_variable(TOKENS_DIMS, name='answer')
+    a = np.random.uniform(size=(5,TOKENS_DIMS)).astype(np.float32)
+
+    loss = C.sequence.reduce_sum(C.cross_entropy_with_softmax(block,A))
+    trainer = C.Trainer(block, (loss, None), C.adam(block.parameters, 0.001, 0.001))
+    print(trainer.train_minibatch(dict(zip(loss.arguments,[t, a]))))
 
     from IPython import embed;embed()
